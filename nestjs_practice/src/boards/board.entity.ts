90544750cfb7b234cd11c103d18ce1f92961e474
Piousangel <yyh7654@gmail.com>
2022-08-26 20:24:10 +0900

    feat : main.ts rewrite

---
+++ nestjs_practice/src/boards/board.entity.ts
@@ -0,0 +1,17 @@
+// import { BaseEntity, Column, PrimaryGenerateColumn} from "typeorm";
+// import { BoardStatus } from "./board.model";
+
+// export class Board extends BaseEntity {
+
+//     @PrimaryGenerateColumn()
+//     id:number;
+
+//     @Column()
+//     title: string;
+
+//     @Column()
+//     description: string;
+
+//     @Column()
+//     staus: BoardStatus;
+// }
---
+++ nestjs_practice/src/boards/board.repository.ts
@@ -0,0 +1,7 @@
+// import { EntityRePository, Repository} from "typeorm";
+// import { Board } from "./board.entity";
+
+// @EntityRePository(Board)
+// export class BoardRepository extends Repository<Board> {
+
+// }
